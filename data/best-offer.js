7f49cbb8d5c5a09467e4ecb475a320aa4ac9943a
Aleksandr <alkushchik@gmail.com>
2020-02-22 07:48:51 +0200

    create project structure

---
+++ data/best-offer.js
@@ -0,0 +1,12 @@
+window.bestOffer = {
+    discount: 15,
+    left: [
+        '8c061815-6a7d-4465-bb78-1bdc6c5adebf', // Only Skinny Jeans
+        'e50a3153-7833-4b85-b412-1a39d215fd38', // Oversized Cardigan
+        'bec71daa-d133-473d-bbb0-1ee0a427a17d' // Only Busted Knee Jean
+    ],
+    right: [
+        '5677f851-1c4a-4e9b-80e9-16d1e6265257', // Levi's Jeans for women,
+        '07cf6ce2-6eee-4e78-a441-f257fdea7ed6' // Boyfriend T-Shirt with Bohemian print
+    ]
+};
---
+++ data/catalog.js
@@ -0,0 +1,197 @@
+window.catalog = [{
+    id: '014c271a-2811-47fc-b63f-ba279a4ec830',
+    dateAdded: '2017-05-15T16:58:40.000Z',
+    title: 'Monki Festival Knitted',
+    description: '',
+    discountedPrice: 24.75,
+    price: 24.75,
+    hasNew: false,
+    category: 'women',
+    fashion: 'Casual style',
+    colors: [],
+    sizes: [],
+    thumbnail: 'thumbnail path', // replace with path to image extracted from catalog layout
+    preview: ['full image path 1', 'full image path 2', 'full image path 3'] // replace with paths to images extracted from item layout
+}, {
+    id: '07cf6ce2-6eee-4e78-a441-f257fdea7ed6',
+    dateAdded: '2017-06-12T15:35:13.000Z',
+    title: 'Boyfriend T-Shirt with Bohemian print',
+    description: '',
+    discountedPrice: 90,
+    price: 120,
+    hasNew: false,
+    category: 'women',
+    fashion: 'Casual style',
+    colors: ['Black', 'White'],
+    sizes: ['UK 18'],
+    thumbnail: 'thumbnail path', // replace with image extracted from item layout
+    preview: ['full image path 1', 'full image path 2', 'full image path 3'] // replace with paths to images extracted from item layout
+}, {
+    id: '0fdfa061-838d-42ab-ae06-99c66115f633',
+    dateAdded: '2017-02-12T11:14:29.000Z',
+    title: 'Boyfriend T-Shirt with Paris Print',
+    description: '',
+    discountedPrice: 85.75,
+    price: 85.75,
+    hasNew: false,
+    category: 'women',
+    fashion: 'Casual style',
+    colors: [],
+    sizes: [],
+    thumbnail: 'thumbnail path', // replace with image extracted from item layout
+    preview: ['full image path 1', 'full image path 2', 'full image path 3'] // replace with paths to images extracted from item layout
+}, {
+    id: '4a3d3c3e-9dc5-4d99-b33d-42b22e20dc0b',
+    dateAdded: '2017-08-02T15:00:40.000Z',
+    title: 'Straight Leg Jeans',
+    description: '',
+    discountedPrice: 55,
+    price: 55,
+    hasNew: false,
+    category: 'women',
+    fashion: 'Nail the 90s',
+    colors: ['Blue'],
+    sizes: ['UK 18'],
+    thumbnail: 'thumbnail path', // replace with image extracted from item layout
+    preview: ['full image path 1', 'full image path 2', 'full image path 3'] // replace with paths to images extracted from item layout
+}, {
+    id: '5677f851-1c4a-4e9b-80e9-16d1e6265257',
+    dateAdded: '2017-07-07T10:00:39.000Z',
+    title: "Levi's Jeans for women",
+    description: '',
+    discountedPrice: null,
+    price: 95.5,
+    hasNew: false,
+    category: 'women',
+    fashion: 'Casual style',
+    colors: ['Blue'],
+    sizes: ['UK 18'],
+    thumbnail: 'thumbnail path', // replace with image extracted from item layout
+    preview: ['full image path 1', 'full image path 2', 'full image path 3'] // replace with paths to images extracted from item layout
+}, {
+    id: '739d3ae0-6dca-4453-a7a4-a94b841a296d',
+    dateAdded: '2017-07-12T09:02:55.000Z',
+    title: 'With Patchwork Crochet',
+    description: '',
+    discountedPrice: 80.6,
+    price: 80.6,
+    hasNew: false,
+    category: 'women',
+    fashion: 'Casual style',
+    colors: [],
+    sizes: [],
+    thumbnail: 'thumbnail path', // replace with image extracted from item layout
+    preview: ['full image path 1', 'full image path 2', 'full image path 3'] // replace with paths to images extracted from item layout
+}, {
+    id: '80d32566-d81c-4ba0-9edf-0eceda3b4360',
+    dateAdded: '2017-01-01T13:26:14.000Z',
+    title: 'Dark classic fit suit',
+    description: 'Featuring fine Italian wool, this elegant suit has pick-stitch edging, cascade buttons at the cuffs',
+    discountedPrice: 180.6,
+    price: 180.6,
+    hasNew: false,
+    category: 'men',
+    fashion: 'Classical style',
+    colors: ['Black', 'Blue'],
+    sizes: ['UK 52', 'UK 54', 'UK 56'],
+    thumbnail: 'thumbnail path', // replace with image extracted from item layout
+    preview: ['full image path 1', 'full image path 2', 'full image path 3'] // replace with paths to images extracted from item layout
+}, {
+    id: '8b300772-eee3-4ff1-b091-e89f17e0e469',
+    dateAdded: '2017-08-10T14:59:00.000Z',
+    title: 'Neck Knitted Jumper',
+    description: '',
+    discountedPrice: 76.25,
+    price: 76.25,
+    hasNew: true,
+    category: 'women',
+    fashion: 'Casual style',
+    colors: ['Green'],
+    sizes: ['UK 19'],
+    thumbnail: 'thumbnail path', // replace with image extracted from item layout
+    preview: ['full image path 1', 'full image path 2', 'full image path 3'] // replace with paths to images extracted from item layout
+}, {
+    id: '8c061815-6a7d-4465-bb78-1bdc6c5adebf',
+    dateAdded: '2017-08-28T09:15:36.000Z',
+    title: 'Only Skinny Jeans',
+    description: '',
+    discountedPrice: 65.5,
+    price: 65.5,
+    hasNew: true,
+    category: 'women',
+    fashion: 'Casual style',
+    colors: ['Phillipa wash'],
+    sizes: ['UK 17'],
+    thumbnail: 'thumbnail path', // replace with image extracted from item layout
+    preview: ['full image path 1', 'full image path 2', 'full image path 3'] // replace with paths to images extracted from item layout
+}, {
+    id: '9ded7821-e510-4a16-ba9f-57c1e3442ad8',
+    dateAdded: '2017-07-19T15:11:04.000Z',
+    title: 'Turtle Neck Jumper in Rib',
+    description: '',
+    discountedPrice: 130.25,
+    price: 130.25,
+    hasNew: false,
+    category: 'women',
+    fashion: 'Casual style',
+    colors: [],
+    sizes: [],
+    thumbnail: 'thumbnail path', // replace with image extracted from item layout
+    preview: ['full image path 1', 'full image path 2', 'full image path 3'] // replace with paths to images extracted from item layout
+}, {
+    id: 'bec71daa-d133-473d-bbb0-1ee0a427a17d',
+    dateAdded: '2017-03-09T17:51:45.000Z',
+    title: 'Only Busted Knee Jean',
+    description: '',
+    discountedPrice: 140.5,
+    price: 140.5,
+    hasNew: false,
+    category: 'women',
+    fashion: 'Casual style',
+    colors: ['Dark Blue'],
+    sizes: ['UK 19'],
+    thumbnail: 'thumbnail path', // replace with image extracted from item layout
+    preview: ['full image path 1', 'full image path 2', 'full image path 3'] // replace with paths to images extracted from item layout
+}, {
+    id: 'ccc8a5d5-7cdf-4706-95f2-abc64761400d',
+    dateAdded: '2017-06-09T17:32:17.000Z',
+    title: 'Colour Block',
+    description: '',
+    discountedPrice: 550.5,
+    price: 550.5,
+    hasNew: false,
+    category: 'women',
+    fashion: 'Casual style',
+    colors: [],
+    sizes: [],
+    thumbnail: 'thumbnail path', // replace with image extracted from item layout
+    preview: ['full image path 1', 'full image path 2', 'full image path 3'] // replace with paths to images extracted from item layout
+}, {
+    id: 'e50a3153-7833-4b85-b412-1a39d215fd38',
+    dateAdded: '2017-04-25T12:14:41.000Z',
+    title: 'Oversized Cardigan',
+    description: '',
+    discountedPrice: 90,
+    price: 90,
+    hasNew: false,
+    category: 'women',
+    fashion: 'Casual style',
+    colors: ['Raw umber', 'Dark blue'],
+    sizes: ['UK 18', 'UK 19'],
+    thumbnail: 'thumbnail path', // replace with image extracted from item layout
+    preview: ['full image path 1', 'full image path 2', 'full image path 3'] // replace with paths to images extracted from item layout
+}, {
+    id: 'ff665110-5e7f-435d-b1b4-651c3d5050ca',
+    dateAdded: '2017-04-05T10:04:16.000Z',
+    title: 'Paul & Joe Sister Jumper with Neon Trims',
+    description: '',
+    discountedPrice: 19.75,
+    price: 19.75,
+    hasNew: false,
+    category: 'women',
+    fashion: 'Casual style',
+    colors: ['Blue'],
+    sizes: ['UK 18'],
+    thumbnail: 'thumbnail path', // replace with image extracted from item layout
+    preview: ['full image path 1', 'full image path 2', 'full image path 3'] // replace with paths to images extracted from item layout
+}];
