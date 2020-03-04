5956c50ad40638126b2fae913177d978e5f1ad04
alekskushchik <alkushchik@gmail.com>
2020-03-04 08:49:02 +0200

    modify responsive

--- js/index.js
+++ js/index.js
@@ -2,26 +2,20 @@
 var products = catalog;
 var shoppingCart = JSON.parse(localStorage.getItem('cartStorage'));
 var newArrivals = document.querySelector('.new-arrivals');
+/////////
+var burgerIcon = document.querySelector(".dropdown-menu-button");
 
-var dropdownMenuButton = document.querySelector('#dropdown-button');
-var closeMenuButton = document.querySelector('#close-dropdown');
-var container = document.querySelector('.wrapper');
+var x = document.querySelector(".navigation-menu");
+burgerIcon.onclick = function () {
+    if (x.style.display === "block") {
+        x.style.display = "none";
+        burgerIcon.classList.toggle('hidden-button')
 
-dropdownMenuButton.addEventListener('click', function() {
-    toggleDropdownMenu();
-});
-
-closeMenuButton.addEventListener('click', function() {
-    toggleDropdownMenu();
-});
-
-function toggleDropdownMenu() {
-    var dropdownMenu = document.querySelector('#dropdown-menu');
-    dropdownMenu.classList.toggle('hidden-menu');
-    container.classList.toggle('fixed-container');
-    dropdownMenuButton.classList.toggle('hidden-button');
-    closeMenuButton.classList.toggle('hidden-button');
-}
+    } else {
+        x.style.display = "block";
+        document.querySelector(".navigation-menu").classList.add('openMenu');
+    }
+};
 
 (function onloadCartTotals() {
     var totals = localStorage.getItem('cartStorage');
